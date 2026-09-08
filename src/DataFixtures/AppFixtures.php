e5cc90f0fbbfe981e431db3ba87053f203fdc699
Darkwuty <magiksith@gmail.com>
2026-09-09 01:00:04 +0200

    fixtures en cours

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,33 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $hasher)
+    {
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $regularUser = new User();
+        $regularUser
+            ->setEmail('freelance@test.local')
+            ->setPassword($this->hasher->hashPassword($regularUser, 'test'));
+
+        $manager->persist($regularUser);
+
+        $adminUser = new User();
+        $adminUser
+            ->setEmail('admin@freelancehub.local')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword($this->hasher->hashPassword($adminUser, 'test'));
+
+        $manager->persist($adminUser);
 
         $manager->flush();
     }
